f75f4fc50758349fa25bb12241f0298734a27718
Robert Guico <rguico@gmail.com>
2014-01-05 08:51:49 -0600

    Use argparse to pass in weight from the command line.

--- cocorahs.py
+++ cocorahs.py
@@ -1,5 +1,10 @@
 #!/usr/local/bin/python
-print('Weight (g): ',end='')
-precipInGrams = input()
-amount = (int(precipInGrams) - 462) / 197
-print('%(amount).02f' % {'amount': amount})
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Calculate water value for provided weight.')
+parser.add_argument("weight", help='Weight (in grams) of the CoCoRaHS canister.', type=float)
+args = parser.parse_args()
+
+amount = (int(args.weight) - 462) / 197
+print('Precip: %(amount).02f' % {'amount': amount})
